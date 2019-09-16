deaee088a851fcf883f9aefb9a3882b6b9613c4b
hvannak <vannak2010@gmail.com>
2019-09-16 15:17:59 +0700

    controll

---
+++ Autherization/Controllers/CtlDiscoveryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autherization.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Autherization.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CtlDiscoveryController : ControllerBase
+    {
+        private readonly IControllerDiscovery _controllerDiscovery;
+        public CtlDiscoveryController(IControllerDiscovery controllerDiscovery)
+        {
+            _controllerDiscovery = controllerDiscovery;
+        }
+
+        [HttpGet]
+        //api/CtlDiscovery
+        public System.Object GetCtlvalue()
+        {
+            var result = _controllerDiscovery.GetControllers();
+            return result;
+        }
+    }
+}
